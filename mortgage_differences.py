51c0138d84ec98e7d038008042419d15da6fa347
jasonyih <jason@Jasons-MacBook-Air-6.local>
2024-04-20 16:30:51 -0400

    Combined APIs and added endpoint to get graph data

--- mortgage_differences.py
+++ mortgage_differences.py
@@ -11,9 +11,84 @@ from fastapi.middleware.cors import CORSMiddleware
 disasters = pd.read_csv("DisasterDeclarationsSummaries.csv")
 mortgages = pd.read_csv("StateMortgagesPercent-30-89DaysLate-thru-2023-09.csv")
 
+state_codes = {
+    "AL": "Alabama",
+    "AK": "Alaska",
+    "AZ": "Arizona",
+    "AR": "Arkansas",
+    "CA": "California",
+    "CO": "Colorado",
+    "CT": "Connecticut",
+    "DE": "Delaware",
+    "FL": "Florida",
+    "GA": "Georgia",
+    "HI": "Hawaii",
+    "ID": "Idaho",
+    "IL": "Illinois",
+    "IN": "Indiana",
+    "IA": "Iowa",
+    "KS": "Kansas",
+    "KY": "Kentucky",
+    "LA": "Louisiana",
+    "ME": "Maine",
+    "MD": "Maryland",
+    "MA": "Massachusetts",
+    "MI": "Michigan",
+    "MN": "Minnesota",
+    "MS": "Mississippi",
+    "MO": "Missouri",
+    "MT": "Montana",
+    "NE": "Nebraska",
+    "NV": "Nevada",
+    "NH": "New Hampshire",
+    "NJ": "New Jersey",
+    "NM": "New Mexico",
+    "NY": "New York",
+    "NC": "North Carolina",
+    "ND": "North Dakota",
+    "OH": "Ohio",
+    "OK": "Oklahoma",
+    "OR": "Oregon",
+    "PA": "Pennsylvania",
+    "RI": "Rhode Island",
+    "SC": "South Carolina",
+    "SD": "South Dakota",
+    "TN": "Tennessee",
+    "TX": "Texas",
+    "UT": "Utah",
+    "VT": "Vermont",
+    "VA": "Virginia",
+    "WA": "Washington",
+    "WV": "West Virginia",
+    "WI": "Wisconsin",
+    "WY": "Wyoming",
+}
+
+def get_disaster_data(disType):
+    data = {}
+    for index, row in disasters.iterrows():
+        incidentType = row['incidentType']
+        state = row['state']
+        if incidentType == disType:
+            if state not in data.keys():
+                data[state] = 1
+            else:
+                data[state] += 1
+    return data
+
+def get_snow_ice_storm():
+    data = {}
+    for index, row in disasters.iterrows():
+        incidentType = row['incidentType']
+        state = row['state']
+        if incidentType == "Snowstorm" or incidentType == "Severe Ice Storm":
+            if state not in data.keys():
+                data[state] = 1
+            else:
+                data[state] += 1
+    return data
+
 def get_df_based_on_date(state, begin_date):
-    parts = begin_date.split("-")
-    begin_date = parts[0] + "-" + parts[1]
     date_t = datetime.strptime(begin_date, "%Y-%m")
     end_date = date_t.replace(year=date_t.year + 1).strftime("%Y-%m")
     new_df = pd.DataFrame()
@@ -24,26 +99,22 @@ def get_df_based_on_date(state, begin_date):
     
     return new_df
 
-def make_graph(data):
-    plt.figure(figsize=(10, 6))
-    for index, row in data.iterrows():
-        plt.plot(row.index, row.values, marker='o', label=index)
+def mortgage_data_for_state(state, date):
+    date_t = datetime.strptime(date, "%Y-%m")
+    end_date = date_t.replace(year=date_t.year + 1).strftime("%Y-%m")
+    data = [
+        ["Date", state]
+    ]
+    
+    row = mortgages[mortgages['Name'] == state_codes[state]]
+    for column in row.columns:
+        if column >= date and column <= end_date:
+            index = row[column].keys()[0]
+            data.append([column, row[column][index]])
+    
+    return data
 
-    # Customize the plot
-    plt.title('Mortgage Delinquency Rates')
-    plt.xlabel('Date')
-    plt.ylabel('Delinquency Rate')
-    plt.xticks(rotation=45)
-    plt.legend(title='Index')
-
-    img_bytes = io.BytesIO()
-    plt.savefig(img_bytes, format='png', bbox_inches='tight')
-    plt.close()
-    img_bytes.seek(0)
-
-    return img_bytes
-
-def get_disasters(state, disaster):
+def disaster_list(state, disaster):
     dis_list = {"list": []}
     for index, row in disasters.iterrows():
         if row['incidentBeginDate'] >= "2010" and row['incidentType'] == disaster and row['state'] == state:
@@ -51,11 +122,6 @@ def get_disasters(state, disaster):
             dis_list["list"].append({"state": row['state'], "incidentType": row['incidentType'], "date": date[:date.index("T")]})
     
     return dis_list
-
-# df = get_df_based_on_date("Texas", "2018-09")
-# img_bytes = make_graph(df)
-# i = Image.open(img_bytes)
-# i.show()
 
 app = FastAPI()
 
@@ -73,13 +139,46 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
+@app.get("/flood_data")
+def get_flood():
+    return get_disaster_data("Flood")
+
+@app.get("/fire_data")
+def get_fire():
+    return get_disaster_data("Fire")
+
+@app.get("/hurricane_data")
+def get_hurricane():
+    return get_disaster_data("Hurricane")
+
+@app.get("/mudslide_data")
+def get_mudslide():
+    return get_disaster_data("Mud/Landslide")
+
+@app.get("/tornado_data")
+def get_tornado():
+    return get_disaster_data("Tornado")
+
+@app.get("/drought_data")
+def get_drought():
+    return get_disaster_data("Drought")
+
+@app.get("/earthquake_data")
+def get_eq():
+    return get_disaster_data("Earthquake")
+
+@app.get("/snowstorm_data")
+def get_snowstorm():
+    return get_snow_ice_storm()
+
 @app.get("/{state}/{disaster}")
 def return_list(state: str, disaster: str):
-    return get_disasters(state, disaster)
+    return disaster_list(state, disaster)
 
 @app.get("/{state}/{disaster}/{date}")
-def return_graph(state: str, disaster: str, date: str):
-    df = get_df_based_on_date(state, date)
-    img_bytes = make_graph(df)
+def return_graph_data(state: str, disaster: str, date: str):
+    return mortgage_data_for_state(state, date)
+    # df = get_df_based_on_date(state, date)
+    # img_bytes = make_graph(df)
 
-    return Response(content=img_bytes.getvalue(), media_type="image/png")
+    # return Response(content=img_bytes.getvalue(), media_type="image/png")
